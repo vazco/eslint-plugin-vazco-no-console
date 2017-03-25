4dad67c3be959de7d445287b3520565b18ff599d
Jan <jan.owsiany@vazco.eu>
2017-03-25 17:33:16 +0100

    Missing comma

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   rules: {
-    'no-console': require('./lib/rules/no-console')
+    'no-console': require('./lib/rules/no-console'),
     'logical-expression-indent': require('./lib/rules/logical-expression-indent')
   },
 };
